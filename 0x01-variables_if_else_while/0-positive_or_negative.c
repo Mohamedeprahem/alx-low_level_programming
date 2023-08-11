dfea6710eb60fc0e71b3c3607926bf0f3250a7b4
mohamedeprahim <hopemib3@gmail.com>
2023-08-11 16:39:50 -0700

    0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,16 @@
+#!/bin/bash
+#include <stdlib.h>
+int main(void)
+{
+	int x = get_int ("input x :");
+	if (x > 0)
+	{
+	printf("is positive");
+	}
+	else if (x == 0)
+	{
+	printf("is zero");
+	{
+	else {
+	printf("is negative"); 
+	}	 
