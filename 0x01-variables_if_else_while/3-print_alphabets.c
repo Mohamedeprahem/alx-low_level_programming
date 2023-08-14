7dbe53b49a9017dfae672915078944ab451d705c
mohamedeprahim <hopemib3@gmail.com>
2023-08-14 16:40:30 -0700

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ * Description : print alphabets
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+char ch = 'a';
+char ch = 'A';
+/*print a - z 
+while (ch <= 'z')
+{
+putchar(ch);
+ch++; 
+}
+/*print A - Z
+while (CH <= 'Z')
+{
+putchar(CH);
+CH++;
+}
+putchar('\n');
+return (0);
+}
