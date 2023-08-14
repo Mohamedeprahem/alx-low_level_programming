219c93a70e784af4e467f7750f4131d9aab484ed
mohamedeprahim <hopemib3@gmail.com>
2023-08-14 15:59:08 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ * Description : print the last digit of the number stored in the variable n.
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+char ch = 'a';
+while (ch<=z);
+putchar (ch);
+ch++;
+putchar('\n');
+return (0);
+{
+
