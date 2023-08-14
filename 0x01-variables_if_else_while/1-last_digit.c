11180249837d2e632ac5a2e281631d4a9629fa1c
mohamedeprahim <hopemib3@gmail.com>
2023-08-14 14:51:53 -0700

    1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,11 +19,11 @@ n = rand() - RAND_MAX / 2;
 int last_digit = n %10;
 
 if (last_digit > 5)
-printf("%d and is greater than 5\n", last_digit)
+printf("%d and is greater than 5\n", last_digit);
 else if (last_digit == 0)
-printf("%d and is 0 \n", last_digit)
+printf("%d and is 0 \n", last_digit);
 else if (last_digit < 6)
-printf("%d and is less than 6 and not 0 \n", last_digit)
+printf("%d and is less than 6 and not 0 \n", last_digit);
 return (0);
 }
 
