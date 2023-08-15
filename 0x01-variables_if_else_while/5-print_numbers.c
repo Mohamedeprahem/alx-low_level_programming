af2d5b6eeebdabc4b4a48846090655b3c6800331
mohamedeprahim <hopemib3@gmail.com>
2023-08-14 17:29:28 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ * Description : print alphabets
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+int number = 0
+while number <= 9
+{
+printf("%d single digit /n", number);
+number = number + 1
+}
+return (0)
+}
