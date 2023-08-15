cc409fb03c7424743568872a3d8f6788b74f3eb1
mohamedeprahim <hopemib3@gmail.com>
2023-08-14 18:10:25 -0700

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ * Description : numberz
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+char ch = 'z';
+while (char >= 'a')
+{
+putchar(ch);
+ch--;
+}
+putchar('\n');
+}
