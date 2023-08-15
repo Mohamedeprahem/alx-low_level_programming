69a28017cf3ba834df56771cb820e3fb6af42add
mohamedeprahim <hopemib3@gmail.com>
2023-08-14 17:53:06 -0700

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ * Description : numberz
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+int num = 0;
+while (num <= 9)
+{
+putchar (num +(0));
+num = num + 1;
+}
+putchar ('\n')
+return (0);
+}
