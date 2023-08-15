2398f9551753c3b3bc7db36bd38338eba9fca8be
mohamedeprahim <hopemib3@gmail.com>
2023-08-14 18:47:34 -0700

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ * Description : numberz
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+int num = 0;
+while (num <= 9)
+{
+putchar(num + 48);
+if (num != 9)
+{
+putchar(',');
+putchar(',');
+}
+num = num + 1;
+}
+putchar('\n');
+return (0);
+}
