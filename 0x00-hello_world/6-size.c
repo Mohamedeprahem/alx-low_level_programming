f9751630375cf26e746d09eb54bad64825bbf227
mohamedeprahim <hopemib3@gmail.com>
2023-08-14 19:02:47 -0700

    1

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/***/
+
+int main(void)
+{
+	printf("size of a",sizeof());
+         printf("size of a",sizeof());
+	 printf("size of a",sizeof());
+	 printf("size of a",sizeof());
+	 printf("size of a",sizeof());
+return (0);
+}	
