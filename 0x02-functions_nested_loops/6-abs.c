5b99225531c36c179563b3a553128cb0cf59380a
mohamedeprahim <hopemib3@gmail.com>
2023-08-16 19:11:11 -0700

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - function that prints the last digit of a number
+ * @n: checks input of function
+ * Return: Always 0
+*/
+
+int _abs(int n)
+{
+if (n < 0)
+n = (-1) * n;
+return (n);
+}
