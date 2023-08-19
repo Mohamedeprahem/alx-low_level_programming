50a9a2fcdd39037ab3957aa16a8b923226a0ebba
mohamedeprahim <hopemib3@gmail.com>
2023-08-18 17:06:04 -0700

    3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,11 +3,13 @@
 /**
  * print_numbers  - prints the numbers,from 0to9
  *
- * Return: Always .
+ * Return: void .
 */
-void print_numbers(void);
+
+void print_numbers(void)
 {
 char i;
+
 for (i = '0'; i <= '9'; i++)
 {
 _putchar(i);
