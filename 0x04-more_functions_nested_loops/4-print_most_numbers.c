165fbfdf8352477cf2a5da97ac882f29ce997fc7
mohamedeprahim <hopemib3@gmail.com>
2023-08-18 17:43:40 -0700

    4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,7 +3,7 @@
 /**
  * print_most_numbers  - prints the numbers,from 0to9 except2,4
  *
- * Return: always 0 (success) .
+ * Return: Always 0 (success) .
 */
 
 void print_most_numbers(void)
@@ -12,7 +12,7 @@ int i;
 
 for (i = 0; i <= 9; i++)
 {
-if (i == 2; || i == 4)
+if (i == 2 || i == 4)
 continue;
 _putchar(i + 48);
 }
