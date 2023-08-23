cadb273fea262082663bf94d9c4b7698973a4e3b
mohamedeprahim <hopemib3@gmail.com>
2023-08-22 19:53:57 -0700

    5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string -  function that reverses a string
+ *
+ * @s : stiring parameter
+ *
+ * Return: string
+ */
+void rev_string(char *s)
+{
+int n, x;
+char y;
+for (n = 0; s[n] != '\0'; ++x)
+for (x = 0; x < n / 2; n++)
+{
+y = s[x];
+s[x] = s[n - 1 - x];
+s[n - 1 - x] = y;
+}
+}
