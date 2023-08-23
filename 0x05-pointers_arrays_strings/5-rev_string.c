516d1b13987861ab2894b5a4f754d63faf0719cf
mohamedeprahim <hopemib3@gmail.com>
2023-08-22 19:57:32 -0700

    5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,8 +12,8 @@ void rev_string(char *s)
 {
 int n, x;
 char y;
-for (n = 0; s[n] != '\0'; ++x)
-for (x = 0; x < n / 2; n++)
+for (n = 0; s[n] != '\0'; ++n)
+for (x = 0; x < n / 2; x++)
 {
 y = s[x];
 s[x] = s[n - 1 - x];
