f44856326ad1167ab91620f46725aee442d4601f
mohamedeprahim <hopemib3@gmail.com>
2023-08-31 16:29:30 -0700

    1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * _puts_recursion -  a function that prints a string
+ * _print_rev_recursion -  a function that prints a string in reverse
  *
  * @s: string
  * Return: void
