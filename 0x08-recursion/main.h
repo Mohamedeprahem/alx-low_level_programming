94ef859d6c769f6c1eee37cbb5812611df0b9e3b
mohamedeprahim <hopemib3@gmail.com>
2023-08-31 15:51:53 -0700

    0

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,7 +2,7 @@
 #define main_h
 #include <stdio.h>
 
-int _putchar(char);
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
