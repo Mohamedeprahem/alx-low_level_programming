95fe9bb2868339eeed3755259c3722e6e722293c
mohamedeprahim <hopemib3@gmail.com>
2023-08-31 19:13:54 -0700

    5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,7 +9,7 @@ int _sqrt_recursion(int n)
 {
 if (n < 0)
 return (-1);
-int modified _sqrt_recursion(int n, 0)
+return  modified _sqrt_recursion(int n, 0)
 }
 /**
  * _sqrt_recursion  - the natural square root of a number
@@ -23,5 +23,5 @@ if (y * y > n)
 return (-1);
 if (y * y == n)
 return (y);
-return  (_sqrt_recursion(n, y + 1));
+return (modified _sqrt_recursion(n, y + 1));
 }
