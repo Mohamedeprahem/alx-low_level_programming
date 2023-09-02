c47930edb6f1648bfb83a5ba3d5ebab60484a12e
mohamedeprahim <hopemib3@gmail.com>
2023-09-01 19:53:24 -0700

    2

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints all arguments it receives
+ * _atoi: numbers of  arguments
+ * @s: array of arguments
+ *
+ * Return: 0
+ */
+
+int _atoi(char *s)
+{
+int x, y, z, w, l, n;
+
+x = 0;
+y = 0;
+z = 0;
+w = 0;
+l = 0;
+n = 0;
+while (s[w] != '\0')
+w++;
+while (x < w && l == 0)
+{
+if (s[x] == '_')
+y++;
+if (s[x] >= '0' && s[x] <= '9')
+{
+n = s[x]- '0';
+if (d % 2)
+n = -n;
+}
+return (0);
+}
