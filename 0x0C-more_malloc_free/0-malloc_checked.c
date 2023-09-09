6da04addb854ae4b497baf4a87f80f2f4368b680
mohamedeprahim <hopemib3@gmail.com>
2023-09-09 10:22:09 -0700

    0Mohamedeprahem

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,10 +10,10 @@
  * Return: Always 0.
  */
 void *malloc_checked(unsigned int b)
-int *a;
-a = malloc(b);
-if (a == 0)
-exit (98);
-return (a);
-
 {
+int *a
+a = malloc(b)
+if (a == 0)
+exit(98);
+return (a);
+}
