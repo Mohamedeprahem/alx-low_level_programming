42887c3af272eefb932c1fbfe3da047a5e9db808
mohamedeprahim <hopemib3@gmail.com>
2023-09-11 15:43:42 -0700

    2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - prints the name of the file it was compiled from, followed by a new line
+ * main - prints the name of the file followed by a new line
  *
- * Return: 0
+ * Return: Always(0)
  */
-int main (void)
+int main(void)
 {
 printf("%s\n", __FILE__);
-return(0);
+return (0);
 }
