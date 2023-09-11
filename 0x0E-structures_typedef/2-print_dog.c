913a2fdaca383cead09f2ca32936213cc3b04075
mohamedeprahim <hopemib3@gmail.com>
2023-09-11 14:21:48 -0700

    2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 
 /**
@@ -12,8 +13,7 @@ if (d == NULL)
 return;
 if (d->name == NULL)
 d->name = "(nil)";
-
 if (d->owner == NULL)
 d->owner = "(nil)";
-printf("name %s\n age %f\n owner %s\n", d->name, d->age, d->owner);
+printf("Name: %s\n Age: %f\n Owner: %s\n", d->name, d->age, d->owner);
 }
