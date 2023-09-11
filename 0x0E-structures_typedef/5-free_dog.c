7333d59137aeca4ba8ea63e80ef6de4aedac9bb1
mohamedeprahim <hopemib3@gmail.com>
2023-09-11 15:12:45 -0700

    5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * free_dog - a function that frees dogs.
+ * @d: dog
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+if (d == NULL)
+{
+if (d->name)
+free(d->name);
+if (d->owner)
+free(d->owner);
+free(d);
+}
+}
