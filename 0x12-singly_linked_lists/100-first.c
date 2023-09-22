6bf7fb63765f3df8c1e4712a69bd83de1ca37012
Mohamedeprahem <hopemib3@gmail.com>
2023-09-21 18:14:34 -0700

    5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void first(void)_attribute_((constructor));
+**
+*first -  prints asentence before the main
+*function is excuted
+*/
+void first(void)
+printf(You're beat! and yet, you must allow,\n);
+printf(I bore my house upon my back!\n);
