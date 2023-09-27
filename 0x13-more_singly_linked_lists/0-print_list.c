2bd541d25f958fa4cb4fd252ae94906c13e9b058
Mohamedeprahem <hopemib3@gmail.com>
2023-09-26 19:11:51 -0700

    0,1,2,3,4

---
+++ 0x13-more_singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - prints all the elements
+ * @h: points to the list  node
+ *
+ * Return: singly linked list node structure
+*/
+
+size_t print_list(const list_t *h)
+{
+
+size_t s = 0;
+while (h)
+{
+if (!h->str)
+printf("[0] (nil)\n");
+else
+printf("[%u] %s\n", h->len, h->str);
+h = h->next;
+s++;
+}
+return (s);
+}
---
+++ 0x13-more_singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements
+ * @h: first node
+ *
+ * Return: size of list
+ */
+size_t list_len(const list_t *h)
+{
+size_t x = 0;
+while (h)
+{
+	h = h->next;
+	x++;
+}
+return (x);
+}
